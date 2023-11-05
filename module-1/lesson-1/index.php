7c516f10a094d42d628ab6d6fece965e54d2ef46
mukhriddin-dev <khodievm@gmail.com>
2023-11-05 19:13:11 +0500

    Lesson 1 v1

--- module-1/lesson-1/index.php
+++ module-1/lesson-1/index.php
@@ -1,9 +1,26 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'Hello world';
-print "Hello world"
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
+<body>
 
+    <?php
+    function myTest()
+    {
+        $x = 5; // local scope
+        echo "<p>Variable x inside function is: $x</p>";
+    }
+    myTest();
 
+    // using x outside the function will generate an error
+    echo "<p>Variable x outside function is: $x</p>";
+    ?>
 
-?>
+</body>
+
+</html>
