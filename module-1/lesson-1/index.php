acd21098fc7dabc86ddfe561cff732c6f1dc0a70
mukhriddin-dev <khodievm@gmail.com>
2024-05-10 05:33:19 +0500

    ok

--- module-1/lesson-1/index.php
+++ module-1/lesson-1/index.php
@@ -10,15 +10,17 @@
 <body>
 
     <?php
+    $x = 5;
     function myTest()
     {
-        $x = 5; // local scope
+        $x = 5;
+        // local scope
         echo "<p>Variable x inside function is: $x</p>";
     }
     myTest();
 
-    // using x outside the function will generate an error
     echo "<p>Variable x outside function is: $x</p>";
+
     ?>
 
 </body>
